a11fe195f84f55874489eed14024410df6dcac10
Almog Manor <ManorAlmog@outlook.com>
2023-04-05 16:08:04 +0300

    version: bump to 1.0.1
    Changes:
     - fix bug where outputted serial had training
       color code information

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ichoose-device',
-    version="1.0.0",
+    version="1.0.1",
     description="Interactivly choose which adb device you want to work with in the current shell session",
     author="Almog Manor",
     author_email="ManorAlmog@outlook.com",
